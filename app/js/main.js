8b73a3ffd147b31ee2e17c9cc2519fa07bc664ee
DeuS7 <never-99@yandex.com>
2019-12-08 18:44:52 +0300

    Deleting non-working stuff

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,31 @@
-console.log("Everything works just fine!");
+//train(getSimpleModel(), data, numEpochs);
+const data = new RPSDataset();
+this.data = data;
+
+const model = getSimpleModel();
+tfvis.show.modelSummary(
+	{ name: 'Simple Model Architecture' },
+	model
+	)
+this.model = model
+
+setTimeout(async () => {
+	await data.load()
+	await showExamples(data)
+}, 10);
+
+//Check untrained
+
+setTimeout(async () => {
+	await showAccuracy(this.model, this.data);
+	await showConfusion(this.model, this.data, 'Untrained Matrix');
+}, 500);
+
+//Train model
+
+setTimeout(async () => {
+	console.log("training begun");
+
+	const numEpochs = 12;
+	await train(this.model, this.data, numEpochs);
+}, 50000);
